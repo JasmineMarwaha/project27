c0362084e6f50161a2ce5d271f5cf6855eb69d6e
JasmineMarwaha <67989737+JasmineMarwaha@users.noreply.github.com>
2020-09-10 19:46:41 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,81 +10,57 @@ function preload()
 }
 
 function setup() {
-	var canvas = createCanvas(1100, 600);
-
-	engine = Engine.create();
-	world = engine.world;
-
-	//Create the Bodies Here.
-
-	roofObject = new Roof(width/2, 50, width, 50);
-	bobObject1 = new Bob(width/2, 350, 50);
-	bobObject2 = new Bob(width/2-100, 350, 50);
-	bobObject3 = new Bob(width/2+100, 350, 50);
-	bobObject4 = new Bob(width/2-200, 350, 50);
-	bobObject5 = new Bob(width/2+200, 350, 50);
-
-	rope1 = new Rope(bobObject1.body, roofObject.body, 0, 0);
-	rope2 = new Rope(bobObject2.body, roofObject.body, -100, 0);
-	rope3= new Rope(bobObject3.body,roofObject.body,+100,0);
-	rope4= new Rope(bobObject4.body,roofObject.body,-200,0);
-	rope5= new Rope(bobObject5.body,roofObject.body,+200,0);
-
-
-	//Engine.run(engine);
-	//rope1 = new Rope(bob);
-	//World.add(world, rope)
-
-}
-
-
-function draw() {
-  background(255);
-  //Engine.update(engine);
-
-  roofObject.display();
-  bobObject1.display();
-  bobObject2.display();
-  bobObject3.display();
-  bobObject4.display();
-  bobObject5.display();
-  rope1.display();
-  rope2.display();
-  rope3.display();
-  rope4.display();
-  rope5.display();
-
-
-  
-  //drawSprites();
- 
-}
-
-function keyPressed() {
-	if(keyCode === UP_ARROW){
-		Body.applyForce(bobObject1.body, bobObject1.body.position, {x:-200, y:100});
+	createCanvas(1100, 600);
+	
+		engine = Engine.create();
+		world = engine.world;
+	   Engine.run(engine);
+		//Create the Bodies Here.
+	
+		roof1= new Roof(width/2,50,width,50);
+		bob1 = new Bob(width/2,350,50);
+		bob2 = new Bob(width/2-100,350,50);
+		bob3 = new Bob(width/2+100,350,50);
+		bob4 = new Bob(width/2-200,350,50);
+		bob5 = new Bob(width/2+200,350,50);
+		chain= new Rope(bob1.body,roof1.body,0,0);
+		chain1= new Rope(bob2.body,roof1.body,-100,0);
+		chain2= new Rope(bob3.body,roof1.body,+100,0);
+		chain3= new Rope(bob4.body,roof1.body,-200,0);
+		chain4= new Rope(bob5.body,roof1.body,+200,0);
+	
+		
+		//rope1 = new Rope(bob);
+		//World.add(world, rope)
+	
 	}
-
-	if(keyCode === UP_ARROW){
-		Body.applyForce(bobObject2.body, bobObject2.body.position, {x:-300, y:200});
+	
+	
+	function draw() {
+	  background(255);
+	 // Engine.update(engine);
+	
+	 roof1.display();
+	 bob1.display();
+	 bob2.display();
+	 bob3.display();
+	 bob4.display();
+	 bob5.display();
+	 chain.display();
+	 chain1.display();
+	 chain2.display();
+	 chain3.display();
+	 chain4.display();
+	  
+	  //drawSprites();
+	 
 	}
-
-	if(keyCode === UP_ARROW){
-		Body.applyForce(bobObject3.body, bobObject3.body.position, {x:-200, y:300});
-	}
-
-	if(keyCode === UP_ARROW){
-		Body.applyForce(bobObject4.body, bobObject4.body.position, {x:300, y:200});
-	}
-
-	if(keyCode === UP_ARROW){
-		Body.applyForce(bobObject5.body, bobObject5.body.position, {x:-100, y:-300});
-	}
-}
-
-
-
-
+	
+	function keyPressed(){
+		if(keyCode==UP_ARROW){Body.applyForce(bob4.body,bob4.body.position,{x:-100,y:-100})};
+	  }
+	  
+	
 
 
 
